d2b4e6e411746d6098d987123e0802addfe157a4
alvaroginer <aginer079j@gmail.com>
2025-03-27 20:06:45 +0100

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,40 @@
 import { useState } from "react";
-//import { OptionalButton } from './optionalButton'
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { Task } from "./task";
 import "./App.css";
 
+//Hacer una barra de búsqueda con preevntdefaul extraer su contenido y con el contenido crear una tarea cuando se pulse en un botón
+// si el input está vacío el botón debe estar disabled
+
 function App() {
-  const [count, setCount] = useState(0);
+  const [taskList, setTaskList] = useState<Task[]>([]);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    setInputValue(e.target.value);
+  };
+
+  const handleClick = () => {
+    setTaskList([
+      ...taskList,
+      { text: inputValue, checked: false, id: new Date() },
+    ]);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form>
+        <input
+          type="text"
+          placeholder="Añadir tarea"
+          value={inputValue}
+          onInput={handleInputChange}
+        />
+        <button onClick={handleClick}>Añadir</button>
+      </form>
+      {taskList.map((task, index) => {
+        <Task text={task.text} id={index} isCompleted={task.checked} />;
+      })}
     </>
   );
 }
---
+++ src/task.tsx
@@ -0,0 +1,21 @@
+interface TaskData {
+  id?: number;
+  text: string;
+  isCompleted: boolean;
+}
+
+export const Task = (props: TaskData) => {
+  const { text, id, isCompleted = false } = props;
+  return (
+    <div key={id} className="task-container">
+      <input
+        type="checkbox"
+        name="checked"
+        checked={isCompleted}
+        onChange={() => !isCompleted}
+      />
+      <p>{text}</p>
+      <button>Eliminar</button>
+    </div>
+  );
+};
