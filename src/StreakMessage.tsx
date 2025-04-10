96dea74baa70f3e9757aca546958948d9ca059e5
alvaroginer <aginer079j@gmail.com>
2025-04-10 08:07:40 +0200

    working on animation

---
+++ src/StreakMessage.tsx
@@ -0,0 +1,15 @@
+export const StreakMessage = () => {
+  return (
+    <div className="">
+      <iframe
+        src="https://gifer.com/embed/6mv"
+        width="480"
+        height="480"
+        frameBorder="0"
+        allowFullScreen
+        title="Streak GIF"
+      ></iframe>
+      <p>You are on a streak!!!!</p>
+    </div>
+  );
+};
